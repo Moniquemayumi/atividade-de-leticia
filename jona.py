aa0a9ecc5cb6a86cfc943304c74364b1c8803fd5
Moniquemayumi <163213589+Moniquemayumi@users.noreply.github.com>
2025-03-18 14:39:43 -0300

    Update jona.py

--- jona.py
+++ jona.py
@@ -1,7 +1,7 @@
 nome = "Davi Brito"
-profissao = "médico"
-doenca = "pele laranja"
+profissao = "medico"
+doenca = "amar demais"
 anosestudando = "15"
-presidente = "Trump"
+presidente = "LULA"
 
 print(f"{nome}, depois de vencer o BBB22 e de estudar cerca de {anosestudando} anos estudando, acabou realizando o seu sonho de se tornar um dos melhores {profissao}. {presidente} estava quase nao resistindo a doença da {doenca}, até que, {nome} comovido com a situação, acabou enfrentando a {doenca} com um pouco de globalização. Essa é a história em que o grande {nome} virou imigrante dos Estados Unidos.")
